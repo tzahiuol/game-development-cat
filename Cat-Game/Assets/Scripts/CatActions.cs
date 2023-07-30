08c216cb9614621fd148ccca0bd2224e81cb3b70
Daniel Swanepoel <d.swanepoel@me.com>
2023-07-30 19:50:05 +0100

    Shove logic added

--- Cat-Game/Assets/Scripts/CatActions.cs
+++ Cat-Game/Assets/Scripts/CatActions.cs
@@ -11,8 +11,9 @@ public class CatActions : MonoBehaviour
     Rigidbody rb;
     ColliosionHandler ch;
     bool shoveItem = false;
+    bool onGround = false;
     [SerializeField] float moveSpeed = 3f;
-    [SerializeField] float turnSpeed = 3f;
+    [SerializeField] float turnSpeed = 0.1f;
     [SerializeField] float jumpForce = 3f;
     // Start is called before the first frame update
     void Start()
@@ -25,10 +26,16 @@ public class CatActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        shoveItem = false;
-        // Debug.Log(transform.forward);
+        
+        
         rb.AddForce(transform.forward * movementVec.z * moveSpeed);
-        rb.AddTorque(transform.up * movementVec.x * turnSpeed);
+        if(onGround){
+            rb.AddTorque(transform.up * movementVec.x * turnSpeed);
+        }
+        
+
+        shoveItem = false;
+        
     }
 
     public void OnMove(InputValue input){
@@ -46,7 +53,7 @@ public class CatActions : MonoBehaviour
     public void OnJump(InputValue input){
         anim.SetTrigger("Jump");
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-        
+        onGround = false;
     }
 
     void OnShove(InputValue input){
@@ -55,10 +62,16 @@ public class CatActions : MonoBehaviour
     }
 
     void OnCollisionStay(Collision other){
+        // Debug.Log(onGround);
         if(other.gameObject.tag == "shoveItem" && shoveItem == true){
             other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
             
         }
+        onGround = true;
+    }
+
+    void OnCollisionExit(){
+        onGround = false;
     }
     
 
