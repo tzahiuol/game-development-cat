9ec28e7a51e9f6b8a27798619fec1dd2abaeedfa
Severin Aebi <severin.aebi@hotmail.de>
2023-09-17 22:08:28 +0200

    Added borders to level 1, fixed collision issues on level 2

---
+++ Cat-Game/Assets/WasherCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasherCollider : MonoBehaviour
+{
+    public Animator washerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            washerAnimator.SetTrigger("WasherUp");
+        }
+    }
+}
